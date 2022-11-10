5f5187599f1b93c5cd8ed6e65ddfe9cb5a05d63e
bookpark <bkbkgg@gmail.com>
2022-11-10 21:06:42 +0900

    Answer 도메인 추가

---
+++ src/main/java/mini/springboard/domain/Answer.java
@@ -0,0 +1,25 @@
+package mini.springboard.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Answer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+    private LocalDateTime createDate;
+
+    @ManyToOne
+    private Question question;
+}
