67d64a3c82b414d5fe3b668e23b847dfb32fa94e
bookpark <bkbkgg@gmail.com>
2022-11-12 22:29:54 +0900

    테스트에 필요한 repository 함수 및 테스트 추가

--- src/main/java/mini/springboard/repository/QuestionRepository.java
+++ src/main/java/mini/springboard/repository/QuestionRepository.java
@@ -3,5 +3,10 @@ package mini.springboard.repository;
 import mini.springboard.domain.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+    Question findBySubject(String subject);
+    Question findBySubjectAndContent(String subject, String content);
+    List<Question> findBySubjectLike(String subject);
 }
