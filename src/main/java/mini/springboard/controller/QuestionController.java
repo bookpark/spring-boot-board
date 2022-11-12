11bea9e5ec0b447c8dcb7e9ff86cde6e196733b2
bookpark <bkbkgg@gmail.com>
2022-11-12 23:42:48 +0900

    질문 상세 페이지를 보여주는 메서드 추가

--- src/main/java/mini/springboard/controller/QuestionController.java
+++ src/main/java/mini/springboard/controller/QuestionController.java
@@ -32,6 +32,8 @@ public class QuestionController {
 
     @RequestMapping("/question/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
+        Question question = this.questionService.getQuestion(id);
+        model.addAttribute("question", question);
         return "question_detail";
     }
 }
