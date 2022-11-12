8c2b64c403fcb36067b821cce8d37889b4dbecb0
bookpark <bkbkgg@gmail.com>
2022-11-12 23:41:23 +0900

    질문 조회 메서드 추가

--- src/main/java/mini/springboard/service/QuestionService.java
+++ src/main/java/mini/springboard/service/QuestionService.java
@@ -3,9 +3,11 @@ package mini.springboard.service;
 import lombok.RequiredArgsConstructor;
 import mini.springboard.domain.Question;
 import mini.springboard.repository.QuestionRepository;
+import mini.springboard.exception.DataNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +16,14 @@ public class QuestionService {
 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if (question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
