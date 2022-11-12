6db4976656376dc4183e429f1a42e1a371cc5757
bookpark <bkbkgg@gmail.com>
2022-11-13 03:28:34 +0900

    질문 등록 기능 추가

--- src/main/java/mini/springboard/controller/QuestionController.java
+++ src/main/java/mini/springboard/controller/QuestionController.java
@@ -6,8 +6,7 @@ import mini.springboard.repository.QuestionRepository;
 import mini.springboard.service.QuestionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.annotation.RequestScope;
 
 import java.util.List;
@@ -36,5 +35,16 @@ public class QuestionController {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String questionCreate() {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String questionCreate(@RequestParam String subject, @RequestParam String content) {
+        this.questionService.create(subject, content);
+        return "redirect:/question/list";
     }
 }
