32a444abccbb4d4e4590183ea8ddf36107894df9
bookpark <bkbkgg@gmail.com>
2022-11-12 22:32:35 +0900

    Question 컨트롤러 추가

---
+++ src/main/java/mini/springboard/controller/QuestionController.java
@@ -0,0 +1,24 @@
+package mini.springboard.controller;
+
+import lombok.RequiredArgsConstructor;
+import mini.springboard.domain.Question;
+import mini.springboard.repository.QuestionRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class QuestionController {
+
+    private final QuestionRepository questionRepository;
+
+    @RequestMapping("/question/list")
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
+    }
+}
